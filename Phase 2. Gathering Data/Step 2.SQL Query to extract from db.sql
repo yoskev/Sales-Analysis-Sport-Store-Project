d8496aa4d2ada64eadd3b391705cdb9435dc3d58
Yosafat <yosafatkev22@gmail.com>
2026-08-05 15:46:18 +0700

    Update Step 2.SQL Query to extract from db.sql
    change version for sql

--- Phase 2. Gathering Data/Step 2.SQL Query to extract from db.sql
+++ Phase 2. Gathering Data/Step 2.SQL Query to extract from db.sql
@@ -1,35 +1,49 @@
 USE distributordatabase;
 
-SELECT SalesOrderID, PatnerStoreID, SalesTeamID, OrderDate, Payment, TotalOrderTrasaction, Note 
+-- 1. Sales Order 2020-2022
+CREATE TEMPORARY TABLE FilteredSalesOrder AS
+SELECT SalesOrderID, PatnerStoreID, SalesTeamID, OrderDate, Payment, TotalOrderTransaction, Note
 FROM SalesOrder
 WHERE OrderDate BETWEEN '2020-01-01' AND '2022-12-31';
 
-SELECT p.ProductID, p.VendorID, p.ProductName, p.Category, p.UnitSalesPrice, p.UnitBuyPrice, p.Quantity, p.UOM, p.DiscountProduct, p.ProductDescription
-FROM Product p
-INNER JOIN OrderItem o on p.ProductID = o.ProductID
-WHERE o.SalesOrderID BETWEEN 'S0001' AND 'S1028';
+SELECT * FROM FilteredSalesOrder;
 
-SELECT v.VendorID, v.VendorName, v.Address, v.City, v.Province, v.email, v.Phone
-FROM Vendor v
-INNER JOIN Product p on v.VendorID = p.VendorID
-WHERE p.ProductID BETWEEN 'PD0001' AND 'PD0020';
-
-SELECT ps.PatnerStoreID, ps.PatnerStoreName, ps.PatnerType, ps.Region, ps.Province, ps.PatnerContactPhone
+-- 2. Patner Store
+SELECT DISTINCT ps.PatnerStoreID, ps.PatnerStoreName, ps.PatnerType, 
+       ps.Region, ps.Province, ps.PatnerContactPhone
 FROM PatnerStore ps
-INNER JOIN SalesOrder s on ps.PatnerStoreIDID = s.PatnerStoreID
-WHERE s.OrderDate BETWEEN '2020-01-01' AND '2022-12-31';
+INNER JOIN FilteredSalesOrder fso ON ps.PatnerStoreID = fso.PatnerStoreID;
 
-SELECT st.SalesTeamID, st.SalesTeamName, st.SalesTeamRegion, st.SalesPromotionMethods, st.TotalProspect 
+-- 3. Sales Team
+SELECT DISTINCT st.SalesTeamID, st.SalesTeamName, st.SalesTeamRegion, 
+       st.SalesPromotionMethods, st.TotalProspect
 FROM SalesTeam st
-INNER JOIN SalesOrder s on st.SalesTeamID = s.SalesTeamID
-WHERE s.OrderDate BETWEEN '2020-01-01' AND '2022-12-31';
+INNER JOIN FilteredSalesOrder fso ON st.SalesTeamID = fso.SalesTeamID;
 
-SELECT o.SalesOrderID, o.ProductID, o.QuantityOrder, o.ItemTotal 
-FROM OrderItem o
-INNER JOIN SalesOrder s on o.SalesOrderID=s.SalesOrderID
-WHERE s.OrderDate BETWEEN '2020-01-01' AND '2022-12-31';
-
-SELECT i.InvoiceID, i.SalesOrderID, i.ShipmentType, i.WarehouseLocation, i.TermsofPayment, i.ShippingCost, i.Taxes, i.TotalDue
+-- 4. Invoice
+SELECT i.InvoiceID, i.SalesOrderID, i.ShipmentType, i.WarehouseLocation, 
+       i.TermsOfPayment, i.ShippingCost, i.Taxes, i.TotalDue
 FROM Invoice i
-INNER JOIN SalesOrder s on i.SalesOrderID = s.SalesOrderID
-WHERE i.SalesOrderID BETWEEN 'S0001' AND 'S1028';
+INNER JOIN FilteredSalesOrder fso ON i.SalesOrderID = fso.SalesOrderID;
+
+-- 5. Order Item
+SELECT oi.SalesOrderID, oi.ProductID, oi.QuantityOrder, oi.ItemTotal
+FROM OrderItem oi
+INNER JOIN FilteredSalesOrder fso ON oi.SalesOrderID = fso.SalesOrderID;
+
+-- 6. Product 
+SELECT DISTINCT p.ProductID, p.VendorID, p.ProductName, p.Category, 
+       p.UnitSalesPrice, p.UnitBuyPrice, p.Quantity, p.UOM, 
+       p.DiscountProduct, p.ProductDescription
+FROM Product p
+INNER JOIN OrderItem oi ON p.ProductID = oi.ProductID
+INNER JOIN FilteredSalesOrder fso ON oi.SalesOrderID = fso.SalesOrderID;
+
+-- 7. Vendor 
+SELECT DISTINCT v.VendorID, v.VendorName, v.Address, v.City, v.Province, v.Email, v.Phone
+FROM Vendor v
+INNER JOIN Product p ON v.VendorID = p.VendorID
+INNER JOIN OrderItem oi ON p.ProductID = oi.ProductID
+INNER JOIN FilteredSalesOrder fso ON oi.SalesOrderID = fso.SalesOrderID;
+
+DROP TEMPORARY TABLE FilteredSalesOrder;
